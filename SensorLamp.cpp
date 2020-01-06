7d14bada371d654e1fb107dc7d1dc0cc736475d9
Stefan Leibold <stefan.leibold@de.bosch.com>
2020-01-06 18:38:25 +0100

    Reworked library for better performance, non blocking

--- SensorLamp.cpp
+++ SensorLamp.cpp
@@ -1,9 +1,12 @@
 #include "SensorLamp.h"
+#include <Arduino.h>
 
 SensorLamp::SensorLamp(uint8_t sensorPin, uint8_t ledPin) :
         filter(FILTERSIZE), sensor_pin(sensorPin), led_pin(ledPin)
 {
-
+    pinMode(sensor_pin, INPUT);
+    pinMode(led_pin, OUTPUT);
+    analogWrite(led_pin, 0);
 }
 
 SensorLamp::~SensorLamp()
@@ -11,12 +14,11 @@ SensorLamp::~SensorLamp()
 
 }
 
-void SensorLamp::init()
+void SensorLamp::init(const uint16_t &senseMin, const uint16_t &senseMax, const uint16_t &trackDistance)
 {
-    pinMode(sensor_pin, INPUT);
-    pinMode(led_pin, OUTPUT);
-
-    analogWrite(led_pin, 0);
+    sense_min = senseMin;
+    sense_max = senseMax;
+    track_distance = trackDistance;
 
     // Fill median filter
     for (uint8_t i = 0; i < filter.size(); i++)
@@ -31,7 +33,7 @@ void SensorLamp::init()
     // Convert analog value to distance in mm
     uint16_t distance = MedianFilter::multiMap(adc_input, in, out, 14);
 
-    // Set max sense value
+    // Adjust max sense value
     if (distance > sense_max)
     {
         ;
@@ -60,177 +62,251 @@ void SensorLamp::init()
             delay(250);
         }
     }
+
+    tracking_lower_limit = sense_min;
+    tracking_upper_limit = sense_max;
 }
 
-void SensorLamp::update(const uint32_t &cycleTime)
+void SensorLamp::update(const uint32_t &cycleTime, const uint32_t &readoutTime, const bool burst)
 {
     uint32_t current_time = millis();
-    static uint32_t last_time = current_time;
-    static uint16_t debounce_cycles = 0;
-    static uint16_t blink_cycles = 0;
-    static uint16_t hand_cycles = 0;
+    static uint32_t debounce_time = 0;
+    static uint32_t tracking_time = 0;
+    static uint32_t blink_time = 0;
 
-    if (current_time - last_time >= cycleTime)
+    // =========================================================================
+    // Measure
+    // =========================================================================
+    if (current_time - last_readout >= readoutTime)
     {
-        last_time = current_time;
+        last_readout = current_time;
+        last_distance = distance;
 
-        // Debounce if neccessary
-        if (debounce_cycles)
+        if (burst)
+          distance = burstMeasure();
+        else
+          distance = measure();
+    }
+
+    // =========================================================================
+    // Debounce
+    // =========================================================================
+    int32_t debounce = (int32_t)(current_time - debounce_time);
+    if (debounce < 0)
+    {
+        return;
+    }
+    else
+    {
+        debounce_time = 0;
+    }
+
+    // =========================================================================
+    // Hand tracking active
+    // =========================================================================
+    if (hand_tracking)
+    {
+        int32_t delta = (int32_t)distance - (int32_t)last_distance;
+        // *********************************************************************
+        // Jump detected
+        // *********************************************************************
+        if ( delta > MAX_JUMP)
         {
-            debounce_cycles -= 1;
-            return;
+            debounce_time = current_time + DEBOUNCE_JUMP_MILLIS;
         }
-
-        // Blink if requested
-        if (blink_cycles)
+        // *********************************************************************
+        // Hand detected
+        // *********************************************************************
+        else if (distance < sense_max)
         {
-            analogWrite(led_pin, 0);
-            blink_cycles -= 1;
-            return;
-        }
-
-        uint16_t distance = measure();
-
-        if (hand_tracking)
-        {
-            if (distance < sense_max)
+            if (distance < tracking_lower_limit)
             {
-                uint8_t hand_tracked_bright = 0;
+                target_bright = MIN_BRIGHTNESS;
 
-                if (distance < tracking_lower_limit)
-                {
-                    hand_tracked_bright = MIN_BRIGHTNESS;
+                // Shift lower border
+                tracking_lower_limit = distance;
+                tracking_upper_limit = constrain(distance + track_distance, sense_min, sense_max);
 
-                    // Shift lower border
-                    tracking_lower_limit = distance;
-                    tracking_upper_limit = constrain(distance + track_distance, sense_min, sense_max);
+                // Blink to signal
+                blink_time = current_time + BLINK_SHORT_MILLIS;
+            }
+            else if (distance > tracking_upper_limit)
+            {
+                target_bright = MAX_BRIGHTNESS;
 
-                    // Blink to signal
-                    blink_cycles = 20 / cycleTime;
-                }
-                else if (distance > tracking_upper_limit)
-                {
-                    hand_tracked_bright = MAX_BRIGHTNESS;
+                // Shift upper border
+                tracking_lower_limit = constrain(distance - track_distance, sense_min, sense_max);
+                tracking_upper_limit = distance;
 
-                    // Shift upper border
-                    tracking_lower_limit = constrain(distance - track_distance, sense_min, sense_max);
-                    tracking_upper_limit = distance;
-
-                    // Blink to signal
-                    blink_cycles = 20 / cycleTime;
-                }
-                else
-                {
-                    hand_tracked_bright = map(distance, tracking_lower_limit, tracking_upper_limit, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
-                }
-
-                if (abs(target_bright - hand_tracked_bright) > HAND_MINIMUM_CHANGE || !lamp_lighted)
-                {
-                    target_bright = hand_tracked_bright;
-                    lamp_lighted = true;
-                }
-
-                hand_cycles = 0;
-                adjustBrightness(target_bright, false);
+                // Blink to signal
+                blink_time = current_time + BLINK_SHORT_MILLIS;
             }
             else
             {
-                hand_cycles += 1;
-                if (hand_cycles == END_TRACKING_CYCLES)
+                if (distance != last_distance)
+                {
+                    target_bright = map(distance, tracking_lower_limit, tracking_upper_limit, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
+                }
+            }
+            tracking_time = 0;
+        }
+        // *********************************************************************
+        // No hand detected
+        // *********************************************************************
+        else
+        {
+            if (tracking_time != 0)
+            {
+                if (current_time - tracking_time >= END_TRACKING_MILLIS)
                 {
                     // Stop hand tracking
                     hand_tracking = false;
-                    hand_cycles = 0;
+                    tracking_time = 0;
 
                     // Save new brightness
                     stored_bright = target_bright;
 
                     // Blink to confirm
-                    blink_cycles = 200 / cycleTime;
+                    blink_time = current_time + BLINK_LONG_MILLIS;
                 }
             }
-        }
-        else
-        {
-            if (distance < sense_max)
+            else
             {
-                hand_cycles += 1;
-                if (hand_cycles == START_TRACKING_CYCLES)
+                tracking_time = current_time;
+            }
+        }
+        // *********************************************************************
+    }
+    // =========================================================================
+    // Hand tracking inactive
+    // =========================================================================
+    else
+    {
+        // *********************************************************************
+        // Hand detected
+        // *********************************************************************
+        if (distance < sense_max)
+        {
+            if (tracking_time != 0)
+            {
+                if (current_time - tracking_time >= START_TRACKING_MILLIS)
                 {
                     // Set tracking range
-                    tracking_lower_limit = constrain(distance - map(stored_bright, 0, 255, 0, track_distance), sense_min, sense_max);
-                    tracking_upper_limit = constrain(distance + map(stored_bright, 0, 255, track_distance, 0), sense_min, sense_max);
+                    tracking_lower_limit = constrain(distance - map(stored_bright, MIN_BRIGHTNESS, MAX_BRIGHTNESS, 0, track_distance), sense_min, sense_max);
+                    tracking_upper_limit = constrain(distance + map(stored_bright, MIN_BRIGHTNESS, MAX_BRIGHTNESS, track_distance, 0), sense_min, sense_max);
 
                     // Start hand tracking
                     hand_tracking = true;
-                    hand_cycles = 0;
+                    tracking_time = 0;
 
                     // Blink to confirm
-                    blink_cycles = 200 / cycleTime;
+                    blink_time = current_time + BLINK_LONG_MILLIS;
                 }
             }
             else
             {
-                if (hand_cycles)
-                {
-                    lamp_lighted = !lamp_lighted;
-                    target_bright = lamp_lighted ? stored_bright : 0;
-                    debounce_cycles = DEBOUNCE_CYCLES;
-                    hand_cycles = 0;
-                }
+              tracking_time = current_time;
             }
-            adjustBrightness(target_bright, true);
         }
+        // *********************************************************************
+        // No hand detected
+        // *********************************************************************
+        else
+        {
+            if (tracking_time != 0)
+            {
+                lamp_lighted = !lamp_lighted;
+                target_bright = lamp_lighted ? stored_bright : 0;
+                debounce_time = current_time + DEBOUNCE_MILLIS;
+                tracking_time = 0;
+            }
+        }
+        // *********************************************************************
+    }
+
+    // =========================================================================
+    // Set brightness or blink if requested
+    // =========================================================================
+    if ((int32_t)(current_time - blink_time) < 0)
+    {
+        analogWrite(led_pin, 0);
+    }
+    else
+    {
+        blink_time = 0;
+        adjustBrightness(target_bright, !hand_tracking, cycleTime);
     }
 }
 
-uint8_t SensorLamp::handDetected(const uint32_t &cycleTime)
+uint8_t SensorLamp::handDetected(const uint32_t &readoutTime, const bool burst)
 {
     uint32_t current_time = millis();
-    static uint32_t last_time = current_time;
-    static uint16_t debounce_cycles = 0;
-    static uint16_t hand_cycles = 0;
+    static uint32_t debounce_time = 0;
+    static uint32_t tracking_time = 0;
 
-    uint8_t detected = 0;
+    uint8_t detected = NOT_DETECTED;
 
-    if (current_time - last_time >= cycleTime)
+    // =========================================================================
+    // Measure
+    // =========================================================================
+    if (current_time - last_readout >= readoutTime)
     {
-        last_time = current_time;
+        last_readout = current_time;
+        last_distance = distance;
 
-        // Debounce if neccessary
-        if (debounce_cycles)
+        if (burst)
+          distance = burstMeasure();
+        else
+          distance = measure();
+    }
+
+    // =========================================================================
+    // Debounce
+    // =========================================================================
+    int32_t debounce = (int32_t)(current_time - debounce_time);
+    if (debounce < 0)
+    {
+        return;
+    }
+    else
+    {
+        debounce_time = 0;
+    }
+
+    // =========================================================================
+    // Hand detected
+    // =========================================================================
+    if (distance < sense_max)
+    {
+        if (tracking_time != 0)
         {
-            debounce_cycles -= 1;
-            return detected;
-        }
-
-        uint16_t distance = measure();
-
-        if (distance < sense_max)
-        {
-            hand_cycles += 1;
-            if (hand_cycles >= START_TRACKING_CYCLES)
+            if (current_time - tracking_time >= START_TRACKING_MILLIS)
             {
-                detected = 2;
+                detected = TRACKING;
             }
         }
         else
         {
-            if (hand_cycles >= START_TRACKING_CYCLES)
+            tracking_time = current_time;
+        }
+    }
+    // =========================================================================
+    // No hand detected
+    // =========================================================================
+    else
+    {
+        if (tracking_time != 0)
+        {
+            debounce_time = current_time + DEBOUNCE_MILLIS;
+            tracking_time = 0;
+
+            if (current_time - tracking_time >= START_TRACKING_MILLIS)
             {
-                debounce_cycles = DEBOUNCE_CYCLES;
-                hand_cycles = 0;
-                detected = 0;
-            }
-            else if (hand_cycles > 0)
-            {
-                debounce_cycles = DEBOUNCE_CYCLES;
-                hand_cycles = 0;
-                detected = 1;
+                detected = TRACKING;
             }
             else
             {
-                detected = 0;
+                detected = DETECTED;
             }
         }
     }
@@ -248,23 +324,55 @@ uint16_t SensorLamp::measure()
     return MedianFilter::multiMap(adc_input, in, out, 14);
 }
 
-void SensorLamp::adjustBrightness(const uint8_t &level, const uint8_t &fade)
+uint16_t SensorLamp::burstMeasure()
 {
-    static uint8_t pwm = 0;
+    // Take our burst readings
+    uint16_t current_reading;
+    uint16_t lowest_reading = 1024;
+
+    for (uint16_t i = 0; i < BURST_READINGS; i++) {
+        // Read analog value
+        current_reading = analogRead(sensor_pin);
+
+        if (current_reading < lowest_reading) {
+            lowest_reading = current_reading;
+        }
+
+        // Delay a short time before taking another reading
+        delayMicroseconds(BURST_DELAY_MICROS);
+    }
+
+    // Add reading to median filter
+    filter.add(lowest_reading);
+    uint16_t adc_input = filter.get();
+
+    // Convert value to distance in mm
+    return MedianFilter::multiMap(adc_input, in, out, 14);
+}
+
+void SensorLamp::adjustBrightness(const uint8_t &level, const bool fade, const uint16_t &fadeTime)
+{
+    uint32_t current_time = millis();
+    static uint32_t last_time = current_time;
 
     // Adjust brightness
-    if (fade && pwm != level)
+    if (fade && pwm_output != level)
     {
-        if (pwm > level && pwm > 0)
-            --pwm;
-        if (pwm < level && pwm < 0xFF)
-            ++pwm;
+        if (current_time - last_time >= fadeTime)
+        {
+            last_time = current_time;
+
+            if (pwm_output > level && pwm_output > 0)
+                --pwm_output;
+            if (pwm_output < level && pwm_output < 0xFF)
+                ++pwm_output;
+        }
     }
     else
     {
-        pwm = level;
+        pwm_output = level;
     }
-    analogWrite(led_pin, pwm);
+    analogWrite(led_pin, pwm_output);
 }
 
 void SensorLamp::getStatus(uint16_t &analog_in, uint8_t &pwm_out)
@@ -273,3 +381,7 @@ void SensorLamp::getStatus(uint16_t &analog_in, uint8_t &pwm_out)
     pwm_out = pwm_output;
 }
 
+uint16_t SensorLamp::getDistance()
+{
+    return distance;
+}
--- SensorLamp.h
+++ SensorLamp.h
@@ -4,16 +4,28 @@
 #include <Arduino.h>
 #include <MedianFilter.h>
 
-#define FILTERSIZE             15    // Sample size for median filter
+#define FILTERSIZE              15    // Sample size for median filter
 
-#define MIN_BRIGHTNESS          1    // Min brightness (0 - 255)
-#define MAX_BRIGHTNESS        255    // Max brightness (0 - 255)
+#define BURST_DELAY_MICROS    1000
+#define BURST_READINGS           8
 
-#define DEBOUNCE_CYCLES        30    // Number of cycles to debounce on / off
-#define START_TRACKING_CYCLES 100    // Delay if hand is detected before starting tracking
-#define END_TRACKING_CYCLES   100    // Delay if no hand is detected while tracking before stopping tracking
-#define HAND_MINIMUM_CHANGE    10    // Minimal brightness change allowed
-#define SENSE_MAX_OFFSET       50    // Defines offset from SENSE_MAX to prevent signal noise from triggering lamp
+#define MIN_BRIGHTNESS           1    // Min brightness (0 - 255)
+#define MAX_BRIGHTNESS         255    // Max brightness (0 - 255)
+
+#define BLINK_SHORT_MILLIS      20
+#define BLINK_LONG_MILLIS      200
+
+#define DEBOUNCE_MILLIS        200
+#define DEBOUNCE_JUMP_MILLIS    50
+#define START_TRACKING_MILLIS  500    // Delay if hand is detected before starting tracking
+#define END_TRACKING_MILLIS    300    // Delay if no hand is detected while tracking before stopping tracking
+
+#define SENSE_MAX_OFFSET        50    // Defines offset from SENSE_MAX to prevent signal noise from triggering lamp
+#define MAX_JUMP               100
+
+#define NOT_DETECTED             0
+#define DETECTED                 1
+#define TRACKING                 2
 
 
 class SensorLamp
@@ -22,12 +34,13 @@ class SensorLamp
     SensorLamp(uint8_t sensorPin = A0, uint8_t ledPin = 13);
     ~SensorLamp();
 
-    void init();
-    void update(const uint32_t &cycleTime = 0);
+    void init(const uint16_t &senseMin = 200, const uint16_t &senseMax = 1500, const uint16_t &trackDistance = 300);
+    void update(const uint32_t &cycleTime = 0, const uint32_t &readoutTime = (BURST_READINGS * BURST_DELAY_MICROS / 1000), const bool burst = true);
 
-    uint8_t handDetected(const uint32_t &cycleTime = 0);
+    uint8_t handDetected(const uint32_t &readoutTime = (BURST_READINGS * BURST_DELAY_MICROS / 1000), const bool burst = true);
 
     void getStatus(uint16_t &analog_in, uint8_t &pwm_out);
+    uint16_t getDistance();
 
   private:
     MedianFilter filter;
@@ -38,19 +51,25 @@ class SensorLamp
     // in[] holds the measured analogRead() values for defined distances
     // Note: The in array should have increasing values
     const int16_t in[14]  = {90, 97, 105, 113, 124, 134, 147, 164, 185, 218, 255, 317, 414, 525};
-    //const int16_t in[14]  = {  68,   70,   74,   78,   83,   87,  91,  94,  97, 101, 105, 145, 330, 340};
+
     // out[] holds the corresponding distances in mm
+    // Note: The out array should have decreasing values
     const int16_t out[14] = {1500, 1400, 1300, 1200, 1100, 1000, 900, 800, 700, 600, 500, 400, 300, 200};
 
     bool lamp_lighted = false;
     bool hand_tracking = false;
 
-    uint16_t sense_min = 200;                   // Minimum sensing height in mm, must not be smaller then min value in out[]
-    uint16_t sense_max = 1500;                  // Maximum sensing height in mm, must not be greater then max value in out[]
-    uint16_t track_distance = 300;              // Distance in mm to move hand to go from min to max brightness
+    uint16_t sense_min;                   // Minimum sensing height in mm, must not be smaller then min value in out[]
+    uint16_t sense_max;                   // Maximum sensing height in mm, must not be greater then max value in out[]
+    uint16_t track_distance;              // Distance in mm to move hand to go from min to max brightness
 
-    uint16_t tracking_lower_limit = sense_min;
-    uint16_t tracking_upper_limit = sense_max;
+    uint16_t tracking_lower_limit;
+    uint16_t tracking_upper_limit;
+
+    uint16_t distance = 0;
+    uint16_t last_distance = 0;
+
+    uint32_t last_readout = 0;
 
     uint8_t pwm_output = 0;
 
@@ -58,7 +77,8 @@ class SensorLamp
     uint8_t target_bright = 0;
 
     uint16_t measure();
-    void adjustBrightness(const uint8_t &level, const uint8_t &fade = true);
+    uint16_t burstMeasure();
+    void adjustBrightness(const uint8_t &level, const bool fade = false, const uint16_t &fadeTime = 0);
 };
 
 #endif // _SENSORLAMP_H_
